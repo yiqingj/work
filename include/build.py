32648d46595e5eca6137bb86ba51cb65d717557c
yiqingj <yiqingj@telenav.com>
2013-11-15 17:37:53 -0800

    add *.h to install path.

--- include/build.py
+++ include/build.py
@@ -14,7 +14,7 @@ if env['GRID_RENDERER']:
 
 if 'install' in COMMAND_LINE_TARGETS:
     for subdir in subdirs:
-        pathdir = os.path.join(base,subdir,'*.hpp')
+        pathdir = os.path.join(base,subdir,'*.hpp','*.h')
         includes = glob(pathdir)
         inc_target = os.path.normpath(env['INSTALL_PREFIX']+'/include/mapnik/'+subdir)
         env.Alias(target='install', source=env.Install(inc_target, includes))
